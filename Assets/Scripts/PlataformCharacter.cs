4c6e0906ab37df760d5a405ab6b4c8bb73e0e846
Ricardo Z Charf <riczcharf@gmail.com>
2015-10-22 14:45:25 -0200

    Initial Commit

---
+++ Assets/Scripts/PlataformCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlataformCharacter : MonoBehaviour {
+
+
+	[HideInInspector]
+	public GameObject closeSwitch = null;
+	public GameObject weaponHandEquip = null;
+	public GameObject itemHandEquip = null;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
