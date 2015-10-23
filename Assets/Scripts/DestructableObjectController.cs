0f8011c8eef2a072f7de2736378ddfa7ad70ae0a
Andre Sakiyama <andre.tsuyoshi@gmail.com>
2015-10-23 13:51:48 -0200

    Prefabs de obetos e scripts interagíveis

---
+++ Assets/Scripts/DestructableObjectController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestructableObjectController : MonoBehaviour {
+
+	//quantidade de dano antes de morrer
+	public int hp = 2;
+
+	//receber dano
+	//param:
+	//	dmg: quantidade de dano recebido
+	public void TakeDamage(){
+		hp -= 1;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hp <= 0) {
+			Destroy();
+		}
+	}
+
+	//DIE!!!!
+	void Destroy() {
+		gameObject.SetActive(false);
+	}
+
+	//Trigger triggered
+	void OnTriggerEnter2D(Collider2D col2D) {
+		if (col2D.gameObject.CompareTag("CharacterWepaon")) { //Tag a definir
+			TakeDamage();
+		}
+	}
+}
---
+++ Assets/Scripts/TriggerObjectController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerObjectController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Função chamada quando o trigger é ativado
+	void Action() {
+
+	}
+
+	void Destroy () {
+		gameObject.SetActive(false);
+	}
+}
